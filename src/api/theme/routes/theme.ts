93baea56def8da87a241c1e680f7e01142950377
Ben Cole <ben@mixcamp.com>
2026-08-20 17:06:07 +0930

    Fix roles.

--- src/api/theme/routes/theme.ts
+++ src/api/theme/routes/theme.ts
@@ -1,3 +1,9 @@
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter("api::theme.theme");
+export default factories.createCoreRouter("api::theme.theme", {
+  config: {
+    find: {
+      auth: false,
+    },
+  },
+});
